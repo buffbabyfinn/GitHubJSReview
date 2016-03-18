864ec551bd1514215d94e87949f0a13a7c1be3b6
Megan <mefayer@gmail.com>
2016-03-18 11:50:21 -0700

    fix front end logic to clear previous entries to fix column height

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
   $('#searchButton').click(function() {
     var user = $('#gitUser').val();
     $('#gitUser').val("");
+    $('#userDisplay').text("");
+    $('#repoDisplay').text("");
     gitMatch(user);
     repoMatch(user);
   });
